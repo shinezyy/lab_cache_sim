1ebb512f139d21ea7470f11b29422cffdf1b0056
Zhou Yaoyang <diamondzyy@sina.com>
2016-04-11 10:25:09 +0800

    fix bug : wrong input method
    when an empty string comes, function str_to_addr handle it as usual

--- src/cache_manage.cpp
+++ src/cache_manage.cpp
@@ -5,6 +5,7 @@
 #include <cstdint>
 #include <cstdio>
 
+#include "debug.h"
 #include "cache.h"
 #include "trace.h"
 #include "latency.h"
@@ -14,12 +15,38 @@ using namespace std;
 uint64_t benchmark_L1(cache *c, vector<char *> *v_trace) { // return cycles
     uint32_t i;
     uint64_t all_cycles = 0;
-    for(i = 0; i < v_trace->size(); i++) {
+    uint32_t trace_size = v_trace->size();
+    log("trace_size is : %d", trace_size);
+    for(i = 0; i < trace_size; i++) {
         uint32_t load, addr, n_cycles;
+
+        uint32_t load2 = 1, addr2, n_cycles2;
+        if((*v_trace)[i][0] == 's') {
+            load2 = 0;
+        }
+        sscanf((*v_trace)[i] + 2, "0x%x %d", &addr2, &n_cycles2);
+
         vector<int> *ret = str_to_addr((*v_trace)[i]);
         load = (*ret)[0];
         addr = (*ret)[1];
         n_cycles = (*ret)[2];
+
+        if(strlen((*v_trace)[i]) <= 1) {
+            return all_cycles;
+        }
+
+        assert(load == load2);
+        if(addr != addr2) {
+            //cout << (*v_trace)[i - 2] << endl;
+            //cout << (*v_trace)[i - 1] << endl;
+            cout << (*v_trace)[i] << endl;
+            log("i is %d",i);
+            log_var(addr);
+            log_var(addr2);
+            exit(0);
+        }
+        assert(n_cycles == n_cycles2);
+
         //printf("%d 0x%x %d\n", load, addr, n_cycles);
         all_cycles += n_cycles;
         all_cycles += L1_LTC;
